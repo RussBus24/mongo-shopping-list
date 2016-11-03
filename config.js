24c45c33bcd43932ac6b02072e95b5871a28a8f9
Russell Henry <rjhenry1014@gmail.com>
2016-11-03 17:49:01 +0000

    Testing of config.js

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
-exports.DATABASE_URL = 'mongodb://<dbuser>:<dbpassword>@ds139937.mlab.com:39937/shopping-list' ||
-                       process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+exports.DATABASE_URL = 'mongodb://<dbuser>:<dbpassword>@ds139937.mlab.com:39937/shopping-list';
+                       
 exports.PORT = process.env.PORT || 8080;
