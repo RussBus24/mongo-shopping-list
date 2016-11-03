53ea87f2a5d8c1c91c81c3d1a543f21bc7b0513c
Russell Henry <rjhenry1014@gmail.com>
2016-11-03 20:45:33 +0000

    Minor correction

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
-exports.DATABASE_URL = 'mongodb://<dbuser>:<dbpassword>@ds139937.mlab.com:39937/shopping-list';
+exports.DATABASE_URL = 'mongodb://<dbuser>:<dbpassword>@ds139937.mlab.com:39937/shopping-list' ||
+                        process.env.DATABASE_URL ||
+                        global.DATABASE_URL ||
+                       (process.env.NODE_ENV === 'production' ? 
+                            'mongodb://localhost/shopping-list' :
+                            'mongodb://localhost/shopping-list-dev');
                        
 exports.PORT = process.env.PORT || 8080;
