e8e7e3cacaddfe636b2520663bdd2fec657db5fe
Russell Henry <rjhenry1014@gmail.com>
2016-11-03 17:43:20 +0000

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.DATABASE_URL = 'mongodb://user:pass@ds139937.mlab.com:39937/shopping-list' ||
+exports.DATABASE_URL = 'mongodb://<dbuser>:<dbpassword>@ds139937.mlab.com:39937/shopping-list' ||
                        process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
