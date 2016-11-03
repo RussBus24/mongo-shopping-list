4ce1c51ea5440db848a68d0561a7cd1577d5c8ac
Russell Henry <rjhenry1014@gmail.com>
2016-11-03 17:20:09 +0000

    Added mLab connection link

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://<dbuser>:<dbpassword>@ds139937.mlab.com:39937/shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
