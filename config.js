990c999981946f239d14d4330f9db21ad79739fa
Russell Henry <rjhenry1014@gmail.com>
2016-11-05 01:25:15 +0000

    Updated file

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
+var dbuser = process.env.dbuser;
+var dbpass = process.env.dbpass;
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                        'mongodb://dbuser:dbpass@ds139937.mlab.com:39937/shopping-list' :
+                        'mongodb://' + dbuser + ':' + dbpass + '@ds139937.mlab.com:39937/shopping-list' :
                         'mongodb://localhost/shopping-list');
 
 exports.PORT = process.env.PORT || 8080;
